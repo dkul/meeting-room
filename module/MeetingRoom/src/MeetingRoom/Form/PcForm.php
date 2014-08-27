eed8a036b0b03f74738faa88953c80371270989d
m.platonov <platonov@f-technology.ru>
2014-08-27 12:26:26 +0400

    Create PC form

---
+++ module/MeetingRoom/src/MeetingRoom/Form/PcForm.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 27.08.14
+ * Time: 12:20
+ */
+
+namespace MeetingRoom\Form;
+
+use Zend\Form\Form;
+
+class PcForm extends Form
+{
+    public function __construct()
+    {
+        parent::__construct('pc');
+        $this->setAttribute('method', 'post');
+        $this->add(array(
+            'name' => 'id',
+            'attributes' => array(
+                'type'  => 'hidden',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'title',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Title',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'title',
+            'attributes' => array(
+                'type'  => 'text',
+            ),
+            'options' => array(
+                'label' => 'Title',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'submit',
+            'attributes' => array(
+                'type'  => 'submit',
+                'value' => 'Go',
+                'id' => 'submitbutton',
+            ),
+        ));
+    }
+} 
