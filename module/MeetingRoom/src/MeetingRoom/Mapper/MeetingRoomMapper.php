7c09bafb208376f041c7df01ae969d527e6961cc
m.platonov <platonov@f-technology.ru>
2014-08-29 10:50:44 +0400

    Add new files

---
+++ module/MeetingRoom/src/MeetingRoom/Mapper/MeetingRoomMapper.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 28.08.14
+ * Time: 14:37
+ */
+
+namespace MeetingRoom\Mapper;
+
+use Application\Mapper\AbstractMapper;
+use Doctrine\ORM\EntityManager;
+
+class MeetingRoomMapper extends AbstractMapper
+{
+    const EntityClass = 'MeetingRoom\Entity\PC';
+
+    public function __construct(EntityManager $em){
+        parent::__construct($em, self::EntityClass);
+    }
+
+    public function getList(){
+        return $this->getEntityManager()->getRepository($this->getEntityClass())->findAll();
+    }
+} 
